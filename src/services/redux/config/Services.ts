b8d70444d0f82bd974d2beac169c58a47b34624d
Prithivi Raj <ptvrajsk@gmail.com>
2021-10-12 17:48:31 +0800

    WIP

---
+++ src/services/redux/config/Services.ts
@@ -0,0 +1,27 @@
+import { createAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import AuthService from "../../Auth/Auth";
+
+// State Data
+interface ServiceState {
+  AuthService?: AuthService
+};
+
+const initialState: ServiceState = {
+  AuthService: undefined
+};
+
+// Redux Slice
+export const servicesReduxSlice = createSlice({
+  name: 'firebase',
+  initialState,
+  reducers: {
+    initAuthService: (state, action: PayloadAction<AuthService>) => {
+      state.AuthService = action.payload
+    }
+  }
+});
+
+// Action Types
+export const { initAuthService } = servicesReduxSlice.actions;
+
+export default servicesReduxSlice.reducer;
